0d5d841944313c069d51118635c1ba9e2db6e995
Joaquin Diaz <jn.diaz@globant.com>
2016-07-07 12:46:33 -0300

    First version button as core component

--- components/app/app.js
+++ components/app/app.js
@@ -6,7 +6,19 @@ var App = React.createClass({
     render: function () {
         return(
             <div className="app">
-                <Button>Click me!</Button>
+                <Button>Default props</Button>
+                <br/>
+                <Button color="primary">Btn color primary</Button>
+                <Button color="secondary">Btn color secondary</Button>
+                <Button color="tertiary">Btn color tertiary</Button>
+                <br/>
+                <Button>Btn big</Button>
+                <Button size="medium" color="secondary">Btn medium</Button>
+                <Button size="small" color="tertiary">Btn small</Button>
+                <br/>
+                <Button buttonType="rounded">R</Button>
+                <Button buttonType="rounded" size="medium" color="secondary">RM</Button>
+                <Button buttonType="rounded" size="small" color="tertiary">RS</Button>
             </div>
         );
     }
--- components/core/button.js
+++ components/core/button.js
@@ -1,11 +1,71 @@
 var React = require('react');
+var classNames = require('classnames');
+
+const colors = ['primary', 'secondary', 'tertiary'];
+const types = ['squared', 'rounded'];
+const sizes = ['big', 'medium', 'small'];
+const block = 'button';
 
 var Button = React.createClass({
+
+    propTypes: {
+        color: React.PropTypes.oneOf(colors),
+        buttonType: React.PropTypes.oneOf(types),
+        size: React.PropTypes.oneOf(sizes)
+    },
+
     render: function () {
         return (
-            <button>{this.props.children}</button>
+            <button className={this.getClass()}>{this.props.children}</button>
         );
+    },
+
+    getClass: function () {
+        return classNames(block, this.getButtonType(), this.getButtonColor(), this.getButtonSize());
+    },
+
+    getButtonType: function () {
+        switch (this.props.buttonType) {
+            case types[0]:
+                return block + '--' + types[0]
+                break;
+            case types[1]:
+                return block + '--' + types[1]
+                break;
+        }
+        return '';
+    },
+
+    getButtonColor: function () {
+        switch (this.props.color) {
+            case colors[0]:
+                return block + '--' + colors[0]
+                break;
+            case colors[1]:
+                return block + '--' + colors[1]
+                break;
+            case colors[2]:
+                return block + '--' + colors[2]
+                break;
+        }
+        return '';
+    },
+
+    getButtonSize: function () {
+        switch (this.props.size) {
+            case sizes[0]:
+                return block + '--' + sizes[0]
+                break;
+            case sizes[1]:
+                return block + '--' + sizes[1]
+                break;
+            case sizes[2]:
+                return block + '--' + sizes[2]
+                break;
+        }
+        return '';
     }
+
 });
 
 module.exports = Button;
