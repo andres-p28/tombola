007638dc20cd0dc8eedfb1b66d5b8a44f8a36a9a
Facundo Mila <facundo.mila@wnco.com>
2016-07-11 13:31:14 -0300

    adding a new button

---
+++ components/core/button-add.js
@@ -0,0 +1,38 @@
+var React = require('react');
+var classNames = require('classnames');
+
+var types = ['primary', 'secondary', 'tertiary'];
+
+var AddButton = React.createClass({
+
+    propTypes: {
+        isRounded: React.PropTypes.bool,
+        buttonType: React.PropTypes.oneOf(types)
+    },
+
+    render: function () {
+        return (
+            <button {...this.getProps()} >{this.props.children}</button>
+        );
+    },
+
+    getProps: function () {
+        return {
+            className: this.getClass(),
+            onClick: this.props.onClick
+        };
+    },
+
+    getClass: function () {
+        return classNames({
+            'button': true,
+            'button-primary': (this.props.buttonType === 'primary'),
+            'button-secondary': (this.props.buttonType === 'secondary'),
+            'button-tertiary': (this.props.buttonType === 'tertiary'),
+            'button--rounded': (this.props.isRounded)
+        }, this.props.className);
+    }
+
+});
+
+module.exports = AddButton;
