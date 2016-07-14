8ac96f102e0da295847a77787c9e27e7dd92a762
Joaquin Diaz <jn.diaz@globant.com>
2016-07-14 15:01:17 -0300

    Api call to local server instead of youtube, duration in videos

---
+++ services/utils.js
@@ -0,0 +1,45 @@
+var Utils = function(){
+
+};
+
+Utils.prototype.parseISO8601ToString = function (date) {
+
+    var reptms = /^PT(?:(\d+)H)?(?:(\d+)M)?(?:(\d+)S)?$/;
+    var hours = '', minutes = '', seconds = '', totalseconds;
+
+    if (reptms.test(date)) {
+      var matches = reptms.exec(date);
+
+      if (matches[1]) {
+          hours = Number(matches[1]);
+
+          if (hours < 10) {
+              hours = '0' + hours;
+          }
+          
+          hours += ':';
+      }
+      if (matches[2]) {
+          minutes = Number(matches[2]);
+
+          if (minutes < 10) {
+              minutes = '0' + minutes;
+          }
+
+          minutes += ':';
+      }
+      if (matches[3]) {
+          seconds = Number(matches[3]);
+
+          if (seconds < 10) {
+              seconds = '0' + seconds;
+          }
+      }
+
+    }
+
+    return hours + minutes + seconds;
+
+};
+
+module.exports = new Utils();
--- services/youtube-service.js
+++ services/youtube-service.js
@@ -1,30 +1,22 @@
+var request = require('reqwest');
 var YoutubeNode = require('youtube-node');
 var Youtube = new YoutubeNode();
 
-var config = {
-    apiKey: 'AIzaSyCyDslI4E40H9dYE_V_18dDlhTCCWOzt4Q',
-    type: 'video',
-    videoDuration: 'short',
-    videoLicense: 'youtube',
-    order: 'viewCount',
-    searchQuantity: 12
+var url = 'http://localhost:3000/api/search';
+
+var YoutubeService = function () {
+
 };
 
-var YoutubeApi = function () {
-    this.youtube = Youtube;
-    this.youtube.setKey(config.apiKey);
-    this.youtube.addParam('type', config.type);
-    this.youtube.addParam('videoDuration', config.videoDuration);
-    this.youtube.addParam('videoLicense', config.videoLicense);
-    this.youtube.addParam('order', config.order);
+YoutubeService.prototype.search = function (searchValue, callBack) {
+    request({
+        url: url,
+        method: 'POST',
+        data: {
+            q: searchValue
+        },
+        success: callBack
+    });
 };
 
-YoutubeApi.prototype.setAPIKey = function (apiKey) {
-    this.youtube.setKey(apiKey);
-};
-
-YoutubeApi.prototype.search = function (searchValue, callBack) {
-    this.youtube.search(searchValue, config.searchQuantity, callBack);
-};
-
-module.exports = new YoutubeApi();
+module.exports = new YoutubeService();
