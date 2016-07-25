76be544dac873da07f069c36cd7c25de4b9fd874
Joaquin Diaz <jn.diaz@globant.com>
2016-07-25 15:20:37 -0300

    Flux integration base

---
+++ dispatcher/app-dispatcher.js
@@ -0,0 +1,14 @@
+// VENDOR LIBS
+var Dispatcher = require('flux').Dispatcher;
+var _ = require('lodash');
+
+var AppDispatcher = _.assign(new Dispatcher(), {
+    handleViewAction: function (action) {
+        this.dispatch({
+            source: 'VIEW_ACTION',
+            action: action
+        });
+    }
+});
+
+module.exports = AppDispatcher;
